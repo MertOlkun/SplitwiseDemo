d91764ced43780cfeac3df9df4308bb394c6e320
Mert Olkun <mert_olkun_26@outlook.com>
2024-11-09 18:07:51 +0300

    Last commit.

--- Program.cs
+++ Program.cs
@@ -12,12 +12,14 @@ class Program
     static void Main(string[] args)
     {
 
-
+        
         UserList userList = new UserList();
 
         SpendingList spendingList = new SpendingList();
 
-        Console.WriteLine("Press: 1 to add user.\nPress: 2 to look at users\nPress: 3 to add Spending\nPress: 4 to look at payment list");
+        var dict = new Dictionary<string, int>();
+
+        Console.WriteLine("\nPress: 1 to add user.\nPress: 2 to look at users\nPress: 3 to add Spending\nPress: 4 to look at payment list");
 
         while (true)
         {
@@ -27,89 +29,88 @@ class Program
             //* ADD USER
             if (a == 1)
             {
-                Console.WriteLine("Enter a name: ");
+                Console.WriteLine("\nEnter a name: ");
                 string? name = Convert.ToString(Console.ReadLine());
 
                 if (name != null)
                 {
                     userList.AddUser(new User(name));
                 }
-                Console.WriteLine("Choose a new action ");
+                Console.WriteLine("\nChoose a new action:\n\nPress: 1 to add user.\nPress: 2 to look at users\nPress: 3 to add Spending\nPress: 4 to look at payment list");
             }
             //* GET USER
             else if (a == 2)
             {
                 userList.GetUser();
-                Console.WriteLine("Choose a new action ");
+                Console.WriteLine("\nChoose a new action:\n\nPress: 1 to add user.\nPress: 2 to look at users\nPress: 3 to add Spending\nPress: 4 to look at payment list");
             }
             //* ADD SPENDING 
             else if (a == 3)
             {
                 List<string> firstPayment = new List<string>();
 
-                Console.WriteLine("What was it spent on");
+                Console.WriteLine("\nWhat was it spent on");
                 string? userSpending = Convert.ToString(Console.ReadLine());
 
                 if (userSpending == null)
                 {
-                    Console.WriteLine("Spending cannot be null.");
+                    Console.WriteLine("\nSpending cannot be null.");
                     break;
                 }
 
                 List<int> payments = new List<int>();
-                var dict = new Dictionary<string, int>();
+                
+                Console.WriteLine("\nWho paid the expense?");
                 string FirstUser = userList.SelectedUser();
-                Console.WriteLine("How much was spent");
+
+                Console.WriteLine("\nHow much was spent?");
                 int WhoPaidSpending = Convert.ToInt32(Console.ReadLine());
                 
 
                 for (int i = 0; i < userList.GetCount(); i++)
                 {
+                    Console.WriteLine("\nAdd payment amount for each user.");
                     string debtorUser = userList.SelectedUser();
-                   /*  System.Console.WriteLine(userList.GetCount()); */
 
+                    Console.WriteLine("\nHow much did the chosen person pay?");
                     int debtorPaid = Convert.ToInt32(Console.ReadLine());
 
-
                     payments.Add(debtorPaid);
-                    /* System.Console.WriteLine(payments[0]);
-                    System.Console.WriteLine(payments.Count()); */
-                    int firsPayment = payments[0];
 
                     int totalDebt = 0;
-
                     for (int s = 0; s < payments.Count(); s++)
                     {
                         int debt = payments[s];
                         totalDebt += debt;
                     }
-                    System.Console.WriteLine(totalDebt);
 
-                    if (totalDebt - firsPayment > firsPayment)
+                    if (totalDebt > WhoPaidSpending)
                     {
-                        Console.WriteLine("Debts cannot exceed payment.");
-                        break;
+                        Console.WriteLine("\nDebts cannot exceed payment.");
+                       break;
                     }
-
-
                     dict.Add(debtorUser, debtorPaid);
                 }
+                
                     
 
                     Spending post = new Spending(FirstUser, userSpending, WhoPaidSpending, dict);
 
                     spendingList.AddSpendings(post);
-                    spendingList.ShowSpendings();
 
-                    Console.WriteLine("Choose a new action ");
+                    Console.WriteLine("\nChoose a new action:\n\nPress: 1 to add user.\nPress: 2 to look at users\nPress: 3 to add Spending\nPress: 4 to look at payment list");
 
             }
             //* VIEW SPENDING LIST
             else if (a == 4)
             {
+                
                 spendingList.ShowSpendings();
-
-                Console.WriteLine("Choose a new action ");
+                foreach (KeyValuePair<string, int> item in dict)
+                {
+                 Console.WriteLine($"Name: {item.Key} Payment: {item.Value}");   
+                }
+                Console.WriteLine("\nChoose a new action:\n\nPress: 1 to add user.\nPress: 2 to look at users\nPress: 3 to add Spending\nPress: 4 to look at payment list");
             }
         }
     }
